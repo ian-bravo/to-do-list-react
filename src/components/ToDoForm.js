9bbfe03717ee53bc8b4a262b5f2955895f61b979
ian-bravo <bravo.ian@gmail.com>
2023-12-12 13:51:35 -0800

    pass the state from the form to the wrapper

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 
-const ToDoForm = () => {
+//after making props in wrapper(parent), pass through addTodo
+const ToDoForm = ({addTodo}) => {
   const [value, setValue] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // need to send state to parent, need to use props (addTodo)
+    addTodo(value)
+    //replace console.log with addTodo after making props in wrapper, and after passing through addTodo
+    setValue("")
+    //setValue("") along with the value={value} will clear the text box
+  }
   return (
-    <form className="ToDoForm">
-      <input type="text" className="toDo-input" placeholder="What is your task?" onChange={ (e) => console.log(e.target.value) }/>
+    <form className="ToDoForm" onSubmit={ handleSubmit }>
+      <input type="text" className="toDo-input" value={value} placeholder="What is your task?" onChange={ (e) => setValue(e.target.value) }/>
+      {/* use console.log(e.target.value) to verify that it works, then use setValue in place of console.log */}
       <button type="submit" className="toDo-btn">Add Task</button>
     </form>
   )
--- src/components/ToDoWrapper.js
+++ src/components/ToDoWrapper.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDoForm from "./ToDoForm";
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
 
 const ToDoWrapper = () => {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = todo => {
+    setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
+    //add props here first, then import prop in ToDoForm
+    console.log(todos)
+  }
   return (
     <div className="ToDoWrapper">
-      <ToDoForm />
+      <ToDoForm addTodo={addTodo}/>
     </div>
   )
 }
