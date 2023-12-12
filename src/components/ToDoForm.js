4c9c39722643366cb17749dec185abef8fdef5d4
ian-bravo <bravo.ian@gmail.com>
2023-12-12 13:30:28 -0800

    add state to form using console.log -> setValue

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ToDoForm = () => {
+  const [value, setValue] = useState("")
   return (
     <form className="ToDoForm">
-      <input type="text" className="toDo-input" placeholder="What is your task?" />
+      <input type="text" className="toDo-input" placeholder="What is your task?" onChange={ (e) => console.log(e.target.value) }/>
       <button type="submit" className="toDo-btn">Add Task</button>
     </form>
   )
